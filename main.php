7ddc17d41f2998519c2a40dc154694c05d06ecb7
El <127678217+e-lsaahale@users.noreply.github.com>
2023-11-22 23:54:56 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binus CurCol</title>
+    
+    <link rel="stylesheet" href="./Assets/mainstyle.css">
+
+
+</head>
+
+<body>
+
+    <div class="top-bar flex align-center" >
+    
+        <a class="bcc-txt">Binus Curcol</a>
+
+        <button class="exit-btn"> Exit </button>
+
+    </div>
+
+    <div class="container">
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+
+        <div class="box">
+            <h2>Username</h2>
+            <p>tes tes tes</p>
+        </div>
+        
+    </div>
+
+    
+
+</body>
+<script src="./Assets/mainscript.js"></script>
+</html>
