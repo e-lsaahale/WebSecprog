04267e022d4479d327577d4482143eb67f7809ad
El <127678217+e-lsaahale@users.noreply.github.com>
2023-11-22 23:55:34 +0700

    Add files via upload

--- Controller/login_auth.php
+++ Controller/login_auth.php
@@ -1,18 +1,14 @@
 <?php
-    //mulai session
+
     session_start();
-    //dia bakal ngambil data login cocokin dr database
+
     require "./conn.php";
 
     $login = false;
 
-    //var penghitungan failed login dan diset 0 countnya
-    $failedlogin = isset($_SESSION['failedlogin']); //? $_SESSION['failedlogin'] : 0;
-
-
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-        $username = $_POST['username'];
+        $username = trim($_POST['username']);
         $length = strlen($username);
         if($length < 6){ 
             header("Location: ../login.php");
@@ -20,10 +16,9 @@
             header("Location: ../login.php");
         }
 
-        //$username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8'); anti xss (example)
-        $pass = $_POST['password'];
 
-        // Countermeasure SQL Injection -> prepared statement
+        $pass = trim($_POST['password']);
+
         $query = "SELECT * FROM users WHERE Username=? AND Passwd=?;";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("ss", $username, $pass);
@@ -31,7 +26,7 @@
         $result = $stmt->get_result();
         $conn->close();
 
-        //Countermeasure illegal access privillege
+    
         if($result->num_rows == 1){
             echo "Login Success";
             $row = $result->fetch_assoc();
@@ -40,31 +35,12 @@
             $_SESSION['username'] = $row['Username'];
             $_SESSION['pass'] = $row['Passwd'];
 
-            // header("Location: "); //ini nanti lempar ke home main page
-
-            $_SESSION['failedLogin'] = 0;
+            header("Location: ../send.php");
         }
         else{
-            //karena gagal, start count failed login
-            $_SESSION['failedLogin']++;
-            $maxFailedAtt = 2;
-
-            if($_SESSION['failedLogin'] >= $maxFailedAtt){
-                //lock account
-                $_SESSION['lockAccess'] = time() + 60 * 1;
-                //further action for new login attempt
-                if(time() < $_SESSION['lockAccess']){
-                    echo "Your account has been locked. Try again later.";
-                }
-                else{
-                    $_SESSION['failedLogin'] = 0;
-                    unset($_SESSION['lockAccess']);
-                }
-            }
-
-            // echo "Login Failed. Try Again.";
-            // header("Location: ../login.php");
+            
             echo "<script>window.location.href='../login.php?error=1';</script>";
+            exit();
         }
     }
 ?>
